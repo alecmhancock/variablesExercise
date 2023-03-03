f2242aef96d998cd4cade59d3604bad8c14b3337
Alec Hancock <alec.m.hancock@gmail.com>
2023-03-02 20:31:04 -0600

    Prelim work on userinput module

--- variablesExercise/Program.cs
+++ variablesExercise/Program.cs
@@ -15,7 +15,7 @@
             double currentWeight = 55.4;
 
             decimal currentHeight = 38.24m;
-            
+
             // implement breaklines for easier viewing
             Console.WriteLine($"My dog, {petName} {lastIntial} is going to the vet for a checkup.\n" +
                 $"He's currently {age} years old, weighs {currentWeight} lbs, and is {currentHeight} inches tall.\n" +
@@ -23,3 +23,24 @@
         }
     }
 }
+
+// Trying to apply basic python knowledge here to create a user input dialog to end the program or ask a question
+//will fix when more info regarding syntax and structure is known
+
+
+    //namespace userInput
+    //{
+      //  public class Program
+        //{
+          //  static void Main(string[] args)
+            //{
+              /// <summary>
+              ///  Console.WriteLine($"Do you have any questions?");
+              /// </summary>
+               // bool yes = true;
+                //bool no = false;
+                //string answer = Console.ReadLine("");
+            //}
+        //}
+    //}
+//}
